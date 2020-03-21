1f9551d128dacfc025c14f8d1bca8a0f2996e776
lixin888888 <moneyloveme8866@gmail.com>
2020-03-21 20:54:28 +0800

    guava 数据集合

---
+++ test4j/src/main/java/com/lixin/guava/MapTest.java
@@ -0,0 +1,68 @@
+package com.lixin.guava;
+
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.HashMultimap;
+import org.junit.Test;
+
+import java.util.Set;
+
+/**
+ * guava 提供的多种map
+ */
+public class MapTest {
+
+    /**
+     * 键值对的双向映射
+     */
+    @Test
+    public void testBiMap(){
+        BiMap<String,String> biMap = HashBiMap.create();
+        biMap.put("sina", "sina.com");
+        biMap.put("qq","qq.com");
+        biMap.put("al","ali.cn" );
+        biMap.forcePut("qq", "qq.cn");
+        BiMap<String, String> inverse = biMap.inverse();
+        for (String value : inverse.keySet()) {
+            System.out.println(value);
+        }
+    }
+
+    /**
+     * 一个键映射多个值
+     */
+    @Test
+    public void testMutiMap(){
+        HashMultimap<String , Integer> multimap = HashMultimap.create();
+        multimap.put("a", 1);
+        multimap.put("a", 2);
+        multimap.put("a",3);
+
+        multimap.put("b",4);
+        multimap.put("b",5);
+
+        System.out.println("multimap get: " + multimap.get("a"));
+        Set<Integer> aset = multimap.get("a");
+        aset.add(7);
+        System.out.println(aset);
+        System.out.println("multimap size: " + multimap.size());
+        System.out.println("multimap keyset size: " + multimap.keySet().size());
+        System.out.println("multimap entries : " + multimap.entries());
+        System.out.println("multimap keys : " + multimap.keys());
+        System.out.println("multimap values : " + multimap.values());
+        System.out.println("multimap asMap : " + multimap.asMap());
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
