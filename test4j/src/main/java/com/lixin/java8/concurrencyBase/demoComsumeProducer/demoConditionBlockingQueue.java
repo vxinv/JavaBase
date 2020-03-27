a9ab355b9a0dbe4d5de8183ac4e07d06221df122
lixin888888 <1377204804@qq.com>
2020-03-27 15:53:52 +0800

    测试 显示条件 多个显示条件之间的协作

---
+++ test4j/src/main/java/com/lixin/java8/concurrencyBase/demoComsumeProducer/demoConditionBlockingQueue.java
@@ -0,0 +1,61 @@
+package com.lixin.java8.concurrencyBase.demoComsumeProducer;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 定义两个等待条件 不满 不空
+ * put时 如果队列满 在notFull 等待 put操作后通知 notEmpty
+ * take时 如果队列空 在notEmpty 等待 take操作后 通知notFull
+ * 这样 代码清晰易读 避免不必要的唤醒和检查 提高效率
+ */
+public class demoConditionBlockingQueue<E> {
+    private Queue<E> queue = null;
+    private int limit;
+    private Lock lock = new ReentrantLock();
+
+    private Condition notFull = lock.newCondition();
+    private Condition notEmpty = lock.newCondition();
+
+    public demoConditionBlockingQueue(int limit) {
+        this.limit = limit;
+        queue = new ArrayDeque<>();
+    }
+    public void put(E e) throws  InterruptedException{
+        lock.lockInterruptibly();
+        try {
+            while (queue.size() == limit){
+                notFull.wait();
+            }
+            queue.add(e);
+            notEmpty.signalAll();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public E take() throws InterruptedException{
+        lock.lockInterruptibly();
+        try {
+            while (queue.isEmpty()){
+                notEmpty.wait();
+            }
+            E poll = queue.poll();
+            notFull.signalAll();
+            return poll;
+        }finally {
+            lock.unlock();
+        }
+    }
+
+}
+
+
+
+
+
+
+
---
+++ test4j/src/main/java/com/lixin/java8/concurrencyBase/demoCondition.java
@@ -0,0 +1,50 @@
+package com.lixin.java8.concurrencyBase;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 使用显示条件进行协作的示例
+ */
+public class demoCondition  extends Thread{
+    private volatile  boolean  fire= false;
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    @Override
+    public void run() {
+        try {
+            lock.lock();
+            System.out.println("start");
+            try {
+                while (!fire) {
+                    condition.await();
+                }
+            }finally {
+                lock.unlock();
+            }
+            System.out.println("fired");
+        }catch (InterruptedException e){
+            Thread.interrupted();
+        }
+    }
+
+    public  void fired(){
+        lock.lock();
+        try {
+            this.fire = true;
+            condition.signalAll();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        demoCondition demoCondition = new demoCondition();
+        demoCondition.start();
+        System.out.println("start demo");
+        demoCondition.fired();
+
+    }
+}
