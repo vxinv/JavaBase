bb4b81823641894e62315b02cddfc268ab5958df
lixin888888 <1377204804@qq.com>
2020-10-16 14:21:40 +0800

    添加文章

---
+++ testweb/src/main/java/com/lixin/testweb/dao/ArticleMapper.java
@@ -0,0 +1,39 @@
+package com.lixin.testweb.dao;
+
+import com.lixin.testweb.model.Article;
+import com.lixin.testweb.model.ArticleExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ArticleMapper {
+    long countByExample(ArticleExample example);
+
+    int deleteByExample(ArticleExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Article record);
+
+    int insertSelective(Article record);
+
+    List<Article> selectByExampleWithBLOBs(ArticleExample example);
+
+    List<Article> selectByExample(ArticleExample example);
+
+    Article selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByExample(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByPrimaryKeySelective(Article record);
+
+    int updateByPrimaryKeyWithBLOBs(Article record);
+
+    int updateByPrimaryKey(Article record);
+}
---
+++ testweb/src/main/java/com/lixin/testweb/model/Article.java
@@ -0,0 +1,63 @@
+package com.lixin.testweb.model;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Article implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    @ApiModelProperty(value = "author")
+    private String author;
+    private LocalDateTime publishTime;
+    @ApiModelProperty(value = "文本")
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(LocalDateTime publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", author=").append(author);
+        sb.append(", publishTime=").append(publishTime);
+        sb.append(", content=").append(content);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
